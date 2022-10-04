b630524bae148adf0d89706e95f187148c961aa0
Bui <silivanxay@gmail.com>
2022-10-04 21:10:35 +0700

    fixed typo

--- apps/deposit/models.py
+++ apps/deposit/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Deposit(models.Model):
-    deposit_amout = models.BigIntegerField()
+    deposit_amount = models.BigIntegerField()
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='deposit')
     updated_on = models.DateTimeField(auto_now=True)
@@ -15,6 +15,6 @@ class Deposit(models.Model):
         verbose_name_plural = ("Deposits")
 
     def __str__(self):
-        return self.name
+        return self.deposit_amount
 
 
