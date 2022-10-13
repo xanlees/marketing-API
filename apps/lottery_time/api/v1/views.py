1ce890f8bc4336b318c5970af3a61d2f3e74f896
Bui <silivanxay@gmail.com>
2022-10-13 21:59:56 +0700

    add lottery time api

--- apps/lottery_time/api/v1/views.py
+++ apps/lottery_time/api/v1/views.py
@@ -7,6 +7,12 @@ class ListCreateAPIView(ListCreateAPIView):
     queryset = Lottery_time.objects.all()
     serializer_class = Lottery_time_Serializer
 
+    def get_serializer(self, *args, **kwargs):
+        """Allows bulk creation of a resource."""
+        if isinstance(kwargs.get('data', {}), list):
+            kwargs['many'] = True
+        return super().get_serializer(*args, **kwargs)
+
     
 class RetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Lottery_time.objects.all()
