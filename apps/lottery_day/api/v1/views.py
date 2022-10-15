855e2702dbb92fd5fa22727a5c30293359892e0e
PernDev <Pern.dev@gmail.com>
2022-10-15 18:54:15 +0700

    create api lottery_day

--- apps/lottery_day/api/v1/views.py
+++ apps/lottery_day/api/v1/views.py
@@ -8,6 +8,12 @@ class ListCreateAPIView(ListCreateAPIView):
     queryset = Lottery_day.objects.all()
     serializer_class = Lottery_daySerializer
 
+    def get_serializer(self, *args, **kwargs):
+        """Allows bulk creation of a resource."""
+        if isinstance(kwargs.get('data', {}), list):
+            kwargs['many'] = True
+        return super().get_serializer(*args, **kwargs)
+
 
 class RetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Lottery_day.objects.all()
