aab8c987fb02b994f5dd277c885898167489b82a
xanleevue <xanleevue20@gmail.com>
2022-10-15 19:42:02 +0700

    change

--- apps/commission/apps.py
+++ apps/commission/apps.py
@@ -1,10 +1,6 @@
 from django.apps import AppConfig
 
 
-<<<<<<< HEAD
-class CommisionConfig(AppConfig):
-=======
 class CommissionConfig(AppConfig):
->>>>>>> test
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'commission'
--- apps/commission/models.py
+++ apps/commission/models.py
@@ -13,11 +13,4 @@ class Commission(models.Model):
     class Meta:
         ordering = ['-created_on']
         verbose_name = ("Commission")
-<<<<<<< HEAD
         verbose_name_plural = ("Commissions")
-
-
-
-=======
-        verbose_name_plural = ("Commissions")
->>>>>>> test
